94469104de3d7c8ab98e822926e36711acead672
Adam Belak <adam_belak@epam.com>
2016-01-29 09:15:58 +0100

    Add JSON mapper and MyExceptionHandler controller advice

--- src/main/java/com/epam/training/canteen/security/config/CustomSuccessHandler.java
+++ src/main/java/com/epam/training/canteen/security/config/CustomSuccessHandler.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
