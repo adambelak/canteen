2ff37ce62cc1fed51ec006425256113ed5e96a72
Adam Belak <adam_belak@epam.com>
2016-01-29 14:25:50 +0100

    Fix DatabaseConfiguration and primary key generation strategy

--- src/main/java/com/epam/training/canteen/config/DatabaseConfiguration.java
+++ src/main/java/com/epam/training/canteen/config/DatabaseConfiguration.java
@@ -2,9 +2,11 @@ package com.epam.training.canteen.config;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.ejb.HibernatePersistence;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,66 +17,52 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 @EnableJpaRepositories("com.epam.training.canteen.*.repository.dao")
 @PropertySource("classpath:database/config.properties")
 public class DatabaseConfiguration {
 
-    @Value("${hibernate.hibernateDialect}")
-    private String hibernateDialect;
-    @Value("${hibernate.showSQL}")
-    private String showSql;
-    @Value("${hibernate.generateStatistics}")
-    private String generateStatistics;
-	
+	@Value("${hibernate.hibernateDialect}")
+	private String hibernateDialect;
+	@Value("${hibernate.showSQL}")
+	private String showSql;
+	@Value("${hibernate.generateStatistics}")
+	private String generateStatistics;
+
 	@Bean(name = "dataSource")
 	public DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder()
-				.setType(EmbeddedDatabaseType.HSQL)
-				.addScript("database/create-db.sql")
-				.addScript("database/insert-data.sql")
-				.build();
+		return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).addScript("database/create-db.sql")
+				.addScript("database/insert-data.sql").build();
 	}
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-        vendorAdapter.setShowSql(true);
 
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setDataSource(dataSource());
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setJpaProperties(getHibernateProperties());
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setShowSql(true);
+		vendorAdapter.setGenerateDdl(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
         factory.setPackagesToScan("com.epam.training.canteen.*.repository.domain");
+		factory.setDataSource(dataSource());
+		factory.afterPropertiesSet();
 
-        return factory;
-    }
-    
-    @Autowired
-    @Bean(name = "transactionManager")
-    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
-        return new HibernateTransactionManager(sessionFactory);
-    }
-    
-    @Autowired
-    @Bean(name = "sessionFactory")
-    public SessionFactory getSessionFactory(DataSource dataSource) {
-		return new LocalSessionFactoryBuilder(dataSource)
-			.addPackage("com.epam.training.canteen.*.repository.domain")
-			.buildSessionFactory();
-    }
-    
-    private Properties getHibernateProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", hibernateDialect);
-        properties.put("hibernate.show_sql", showSql);
-        properties.put("hibernate.cache.use_second_level_cache", false);
-        properties.put("hibernate.generate_statistics", generateStatistics);
-        return properties;
-    }
-	
+		return factory.getObject();
+	}
+
+	@Bean(name = "transactionManager")
+	public PlatformTransactionManager transactionManager() {
+
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory());
+		return txManager;
+	}
+
 }
--- src/main/java/com/epam/training/canteen/menu/repository/domain/FlavorEntity.java
+++ src/main/java/com/epam/training/canteen/menu/repository/domain/FlavorEntity.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,7 +16,7 @@ public class FlavorEntity implements Serializable {
 	private static final long serialVersionUID = 4836015177372501468L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(nullable = false, unique = true)
