39687cd20a863f90dbe4ba60bdc57dd2f229c43b
Adam Belak <adam_belak@epam.com>
2016-01-29 17:21:15 +0100

    Hide alert after 3 seconds

--- src/main/webapp/WEB-INF/assets/js/app.js
+++ src/main/webapp/WEB-INF/assets/js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-	$('body').on('hidden.bs.modal', '.modal', function () {
-	    $(this).removeData('bs.modal');
+	$('body').on('hidden.bs.modal', '.modal', function() {
+		$(this).removeData('bs.modal');
+	});
+	$('#alerts').bind("DOMSubtreeModified", function() {
+		function isEmpty( el ){
+			return !$.trim(el.html())
+		}
+		if (!isEmpty($('#alerts'))) {
+			setTimeout(function() {
+				$(".alert", $('#alerts')).each(function(element) {
+					$(this).alert('close');
+				});
+				setTimeout(function() {
+					$('#alerts').empty();
+				}, 600);
+			}, 3000);
+		}
 	});
 });
